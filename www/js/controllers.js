a2b4389393bff3af3d772eb85ef8e155f4de7e15
scaphandroid <arl-informatique@gmx.ch>
2017-01-31 09:37:34 +0100

    src pour play

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('starter.controllers', [])
 .controller('LoginCtrl', function($scope, $state) {
 
   $scope.signIn = function() {
-  $state.go('tab.courses');
+    $state.go('tab.courses');
   }
 })
 
@@ -15,8 +15,8 @@ angular.module('starter.controllers', [])
     console.log($scope.courses);
   });
 
-  $scope.audioPlayer = function() {
-     console.log('play');
+  $scope.audioPlayer = function(src ) {
+     console.log('play ' + src);
   }
 })
 
