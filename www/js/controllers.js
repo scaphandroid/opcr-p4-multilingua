49179217372b2b66aea0e9fdef8ae42065532637
scaphandroid <arl-informatique@gmx.ch>
2017-01-31 09:40:16 +0100

    ajout commentaires

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,7 @@ angular.module('starter.controllers', [])
 
 .controller('CoursesCtrl', function($scope, Courses) {
 
-  // on récupère la leçon en fonction du language et de la date désirée via le service Courses
+  // on récupère la leçon en fonction du language et de la date désiré via le service Courses
   Courses.get('eng', '').success( function (response) {
     $scope.courses = response;
     console.log($scope.courses);
@@ -22,6 +22,7 @@ angular.module('starter.controllers', [])
 
 .controller('PlanningCtrl', function($scope, Planning) {
 
+  // on récupère le planning en fonction du language désiré via le service Planning
   Planning.get('eng').success( function (response) {
     $scope.planning = response;
     console.log($scope.planning);
