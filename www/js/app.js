e0ed382462bba5fa78a6979778aaa9c7588b27ab
scaphandroid <arl-informatique@gmx.ch>
2017-02-13 17:52:17 +0100

    directive ng enter et utilisation sur le champs password pour déclencher le login

--- www/js/app.js
+++ www/js/app.js
@@ -119,4 +119,18 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('login');
 
+    })
+
+    .directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.ngEnter);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
     });
